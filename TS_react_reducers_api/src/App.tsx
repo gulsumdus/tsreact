51c181afbffc93961a03e3562c54a633561e7ff9
gulsumdus <dusgulsum@gmail.com>
2025-03-18 16:51:46 +0300

    Update App.tsx

--- TS_react_reducers_api/src/App.tsx
+++ TS_react_reducers_api/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
  
   return (
     <>
-     <Counter>{(num: number) =><> Current Count: {num} </>}</Counter>
+     {/* <Counter>{(num: number) =><> Current Count: {num} </>}</Counter> */}
     </>
   )
 }
